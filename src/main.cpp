035efcc01b88c25775af64ed0212b50f4bbb3b25
Astrid Yu <astrid@astrid.tech>
2021-01-08 13:25:59 -0800

    Confirm the MOSFET works

--- src/main.cpp
+++ src/main.cpp
@@ -6,6 +6,10 @@
 
 SoftwareSerial bt(PIN_BT_TX, PIN_BT_RX);
 
+bool is_connected() { return digitalRead(PIN_BT_STATE); }
+
+void set_motor(int level) { analogWrite(PIN_MOTOR, level); }
+
 void setup() {
     Serial.begin(9600);
     bt.begin(38400);
@@ -14,21 +18,29 @@ void setup() {
 
     pinMode(PIN_BT_EN, OUTPUT);
     pinMode(PIN_BT_STATE, INPUT);
-    digitalWrite(PIN_BT_EN, 0);
-
     pinMode(PIN_LED, OUTPUT);
+    pinMode(PIN_MOTOR, OUTPUT);
+
+    set_motor(0);
+    digitalWrite(PIN_BT_EN, 0);
     digitalWrite(PIN_LED, 1);
+
     {
         HC05_ATMode at(&bt, PIN_BT_EN, PIN_BT_STATE);
         delay(10);
         at.set_name("Vibe Check");
-        at.set_pin("5432");
+        at.set_pin("8763");
         at.set_role(HC05_Role_Slave);
-
-        delay(1000);
-
-        Serial.println(at.get_name());
     }
+
+    set_motor(1023);
 }
 
-void loop() {}
+void loop() {
+    while (!is_connected()) {
+        delay(500);
+    }
+    bt.println("OK");
+    while (is_connected()) {
+    }
+}
