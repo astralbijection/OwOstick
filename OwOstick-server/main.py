ef4f49140f6442bd9ff255231303571bf213008f
Astrid Yu <astrid@astrid.tech>
2021-01-15 00:43:03 -0800

    Socket forwarding

--- OwOstick-server/main.py
+++ OwOstick-server/main.py
@@ -1,25 +1,73 @@
 import tornado
 import tornado.websocket
+import json
+import logging
 
 
-class WSHandler(tornado.websocket.WebSocketHandler):
+logger = logging.getLogger(__name__)
+controller = None
+device = None
+
+
+class ControllerHandler(tornado.websocket.WebSocketHandler):
     def open(self):
-        print('new connection')
+        global controller
+        logger.info("Controller connected: %s", self)
+        if controller is not None:
+            logger.warn("Controller already exists! Closing.")
+            self.close()
+            return
+        controller = self
+
+    def on_message(self, message):
+        global device
+        if device is None:
+            return
+        logger.info("Got message: %s", message)
+        obj = json.loads(message)
+        logger.debug("Decoded message to object: %s", obj)
+        action = obj.get('action')
+        if action == 'set_power':
+            value = float(obj.get('value'))
+            logger.debug("Received set power command value=%s", value)
+            device.set_power(value)
+
+    def on_close(self):
+        global controller
+        if controller == self:
+            controller = None
+
+
+class DeviceHandler(tornado.websocket.WebSocketHandler):
+    def set_power(self, value):
+        self.write_message(json.dumps({'action': 'set_power', 'value': value}))
+
+    def open(self):
+        global device
+        if device is not None:
+            self.close()
+        device = self
         self.write_message("Hello World")
 
     def on_message(self, message):
-        print('message received %s' % message)
+        global controller
+        if controller is None:
+            return
 
     def on_close(self):
-        print('connection closed')
+        global device
+        if device == self:
+            device = None
 
 
 application = tornado.web.Application([
-    (r'/ws', WSHandler),
+    (r'/api/controller', ControllerHandler),
+    (r'/api/device', DeviceHandler),
 ])
 
 
 if __name__ == "__main__":
+    logger.setLevel(logging.DEBUG)
     http_server = tornado.httpserver.HTTPServer(application)
     http_server.listen(8888)
     tornado.ioloop.IOLoop.instance().start()
